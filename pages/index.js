9f6365e15522f116439d94f4a154608f1244c96e
Dejohn Huffman <dejohnhuffman1@gmail.com>
2020-09-15 21:25:17 -0400

    home screen

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import Link from 'next/link'
+
 import styles from '../styles/Home.module.css'
 import {FontAwesomeIcon}from '@fortawesome/react-fontawesome'
 import {faSearchPlus}from '@fortawesome/free-solid-svg-icons'
@@ -10,24 +12,35 @@ export default function Home() {
         <title>:</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className={styles.main}>
+    <div className={styles.p2p}>
+    <h3>Poll2Poll</h3>
+    </div>
+      <div className={styles.pollbox}>
         <h1 className={styles.title}>
           Check Poll
         </h1>
         <h1 className={styles.title}>
           Status
         </h1>
-
+    
+    
         <p className={styles.description}>
             <label for="name">Enter Zip:</label>
-
+      
 <input type="number" id="name" name="name" required
        minLength="5" maxLength="5" size="10"></input>
-        
-       <div className={styles.fontW}> <FontAwesomeIcon icon={faSearchPlus} />
+         
+      <div className={styles.fontW}> 
+        <Link href="/listings">
+          <a><FontAwesomeIcon icon={faSearchPlus} /></a>
+        </Link>
+
         </div></p> 
+      </div>
+        
         </main> 
+        
     </div>
   )
 }
