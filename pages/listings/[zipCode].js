1808ddbd9cfd16d2615847f84df7f3dccc5057e0
Dejohn Huffman <dejohnhuffman1@gmail.com>
2020-09-26 09:39:17 -0400

    newupdate4

--- pages/listings/[zipCode].js
+++ pages/listings/[zipCode].js
@@ -1,11 +1,16 @@
 import { useRouter } from 'next/router'
 import useLocationSearch from '../../hooks/useLocationSearch'
+import styles from '../../styles/Home.module.css'
+import Head from 'next/head'
+import Link from 'next/link'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
 
 const roundNumber = (number) => {
     return Math.round(number * 100) / 100;
 }
 
-const ListingEntry = ({locData}) => {
+const ListingEntry = ({ locData }) => {
     return (
         <li key={locData.location.id}>Name: {locData.location.name} ({roundNumber(locData.distance)} miles away)</li>)
 }
@@ -22,11 +27,35 @@ export default function Listings() {
     const locations = (searchResult.locations || [])
 
     return (
-        <div>
-            <h1>Listing</h1>
-            <ul>
-                {locations.map((locData) => <ListingEntry locData={locData} />)}
-            </ul>
+        <div className={styles.container}>
+            <Head>
+                <title>Poll2Poll</title>
+            </Head>
+            <div className={styles.p2p}>
+                <h3 className={styles.p2p1}>Poll2Poll <br />: City</h3>
+                <div className={[styles.fontW, styles.p2p2].join(" ")}>
+                    <Link href="/">
+                        <a><FontAwesomeIcon icon={faHome} /></a>
+                    </Link>
+
+                </div>
+            </div>
+            <main className={styles.main}>
+                <div className={styles.powderblue}>
+                    <p className={styles.deepblue}>your closest Polling
+                    Station for {zipCode}
+            </p>
+
+                    <div className={styles.pollbox}>
+                        <p>
+                            ADDRESS HERE
+            </p>
+                    </div>
+                </div>
+
+            </main>
+
         </div>
     )
+
 }
